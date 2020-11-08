4a322211b25c36ec35772abfdc8f2fcb9dda1a22
cj <power4j@outlook.com>
2020-11-08 19:59:24 +0800

    Update oss-release.sh

--- oss-release.sh
+++ oss-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean deploy -Dgpg.passphrase=${GPG_PWD} -DskipTests=true -P 'oss-release,aliyun-repo'
+./mvnw clean deploy -Dgpg.passphrase=${GPG_PWD} -DskipTests=true -P 'oss-release,aliyun-repo'
