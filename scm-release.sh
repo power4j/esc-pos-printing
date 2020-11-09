eb51c3536554e231a2a7f218b1f5cb493a899a76
cj <power4j@outlook.com>
2020-11-09 14:21:30 +0800

    chore: update scm-release.sh

--- scm-release.sh
+++ scm-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mvn release:clean -P 'oss-release,aliyun-repo' && \
-mvn release:prepare -P 'oss-release,aliyun-repo' && \
-mvn release:perform -P 'oss-release,aliyun-repo'
+./mvnw  release:clean -P 'oss-release,aliyun-repo' && \
+./mvnw  release:prepare -P 'oss-release,aliyun-repo' && \
+./mvnw  release:perform -P 'oss-release,aliyun-repo'
